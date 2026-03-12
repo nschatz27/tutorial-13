04bc6a47a0ec32e6062cd6c4c7c728b9b1dc394b
nschatz27 <akschatz1s@capetigers.com>
2026-03-12 10:45:04 -0500

    fix and compare for errors

--- js/co_order.js
+++ js/co_order.js
@@ -7,7 +7,7 @@
 
    Order Form Script
    
-   Author: Noah Schatz -
+   Author: Noah Schatz
    Date:   03/09/2026
    
    Filename: co_order.js
@@ -28,23 +28,24 @@
    
 */
 
-  window.addEventListener("load", function() {
+  window.addEventListener("load", function () {
       var orderForm = document.forms.orderForm;
       orderForm.elements.orderDate.value = new Date().toDateString();
       orderForm.elements.model.focus();
 
       // Calculate the cost of the order
       calcOrder();
-   });
-
-   // Event Handlers for the web form
+       // Event handlers for the web form
    orderForm.elements.model.onchange = calcOrder;
    orderForm.elements.qty.onchange = calcOrder;
+   var planOptions = document.querySelectorAll('input[name= "protection"]');
 
-   var planOptions = document.querySelectorAll('input[name="protection"]');
    for (var i = 0; i < planOptions.length; i++) {
       planOptions[i].onclick = calcOrder;
    }
+   });
+
+
 
    function calcOrder() {
       var orderForm = document.forms.orderForm;
@@ -56,18 +57,18 @@
       var quantity = orderForm.elements.qty[qIndex].value;
 
       // Initial cost = model cost x quantity
-      var initialCost = mCost*quantity;
+      var initialCost = mCost * quantity;
       orderForm.elements.initialCost.value = formatUSCurrency(initialCost);
 
       // Retrieve the cost of the user's protection plan
-      var pCost = document.querySelector('input[name="protection"]:checked').value;
+      var pCost = document.querySelector('input[name="protection"]:checked').value * quantity;
       orderForm.elements.protectionCost.value = formatNumber(pCost, 2);
 
       // Calculate the order subtotal
       orderForm.elements.subtotal.value = formatNumber(initialCost + pCost, 2);
 
       // Calculate the sales tax
-      var salesTax = 0.05*(initialCost + pCost);
+      var salesTax = 0.05 * (initialCost + pCost);
       orderForm.elements.salesTax.value = formatNumber(salesTax, 2);
 
       // Calculate the cost of the total order
@@ -75,17 +76,18 @@
       orderForm.elements.totalCost.value = formatUSCurrency(totalCost);
 
       // Store the order details
-      orderForm.elements.modelName.value =
-         orderForm.elements.model.options[mIndex].text;
-      orderForm.elements.protectionName.value =
-         document.querySelector('input[name="protection"]:checked').nextSibling.nodeValues;
+      orderForm.elements.modelName.value = orderForm.elements.model.options[mIndex].text;
+      orderForm.elements.protectionName.value = document.querySelector('input[name="protection"]:checked').nextSibling.nodeValue;
+   }
 
    function formatNumber(val, decimals) {
-      return val.toLocaleString(undefined, {minimumFractionDigists: decimals, maximumFractionDigists: decimals});
+      return val.toLocaleString(undefined,
+         {
+         minimumFractionDigits: decimals,
+         maximumFractionDigits: decimals
+      });
    }
 
    function formatUSCurrency(val) {
-      return val.toLocaleString('en-US', {style: "currency", currency: "USD"} );
+      return val.toLocaleString('en-US', { style: "currency", currency: "USD" });
    }
-
-}
